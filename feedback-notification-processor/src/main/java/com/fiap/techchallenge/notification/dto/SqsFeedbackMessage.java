662dfaa10c57d269a48e961f882902c30613fbe1
Gustavo Leite <gustavo.jog@hotmail.com>
2025-12-08 21:20:22 -0300

    feat: add notification processor

---
+++ feedback-notification-processor/src/main/java/com/fiap/techchallenge/notification/dto/SqsFeedbackMessage.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.notification.dto;
+
+public class SqsFeedbackMessage {
+    private String feedbackId;
+
+    public String getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(String feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+}
